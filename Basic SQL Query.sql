84c513f8e55a66300080bccfe1acaa0c10ea9888
subbuworkspace <111069112+subbuworkspace@users.noreply.github.com>
2025-03-19 23:56:51 +0530

    Update Basic SQL Query.sql

--- Basic SQL Query.sql
+++ Basic SQL Query.sql
@@ -1,5 +1,13 @@
 CREATE DATABASE Barirbosta;
 
+--MAX
+select*from Sales
+
+select max (Totalamount) [Maximum amount] from [dbo].[Sales]
+
+select max (SaleDate) [Maximum salesdate] from [dbo].[Sales]
+
+select max (PaymentMethod) [Maximum paymethod] from [dbo].[Sales]
 
 
 
