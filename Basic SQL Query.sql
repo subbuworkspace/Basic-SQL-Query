4c0b6656307e046f3a27f8610aedd97759f084c6
subbuworkspace <111069112+subbuworkspace@users.noreply.github.com>
2025-03-19 23:59:07 +0530

    Update Basic SQL Query.sql

--- Basic SQL Query.sql
+++ Basic SQL Query.sql
@@ -9,7 +9,9 @@ select max (SaleDate) [Maximum salesdate] from [dbo].[Sales]
 
 select max (PaymentMethod) [Maximum paymethod] from [dbo].[Sales]
 
-
+--maximum quamtity sold for each product
+select productid, max (Quantity) [Maximum quantity] from [dbo].[Sales]
+group by ProductID
 
 
 
